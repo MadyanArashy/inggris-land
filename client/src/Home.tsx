64df5c509a43b17461d67e48d52977ae016e7d34
MadyanArashy <madyan.arashy5@smk.belajar.id>
2025-05-03 17:14:05 +0700

    chore: start work on pages and components

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,9 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <>
-      <div>
-        <img src="https://placehold.co/400x300" alt="" className="object-cover w-full lg:h-128" />
+      <div className="relative w-full pt-48 lg:pt-0 lg:h-104 bg-cover bg-no-repeat bg-position-[center_20%] bg-[url('/home-cover-1.png')]">
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+
+        {/* Text */}
+        <div className="absolute lg:top-56 lg:left-4 bg-white px-8 py-4 z-20 rounded-tl-3xl rounded-br-3xl flex flex-col gap-4 lg:w-md">
+          <h1 className="text-xl md:text-3xl font-bold text-center">
+            Helping you learn English, step-by-step
+          </h1>
+          <hr className="border-4 -mx-8 border-primary-500"/>
+          <p>
+            Want to improve your English? Feel like the other options just don't fit your style?
+            Try out <strong className="text-primary-500 font-bold">InggrisLand</strong>,
+            a free English-learning app designed to help out people just like you
+            ! <Link to={"/explore"} className="text-primary-500 hover:underline focus:underline">Click here to try!</Link>
+          </p>
+        </div>
       </div>
+
     </>
   )
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,29 +1,42 @@
 import "../index.css";
 import { Link } from "react-router-dom";
+import logo from "../assets/inggrisland-logo.png";
 
 function Navbar() {
   return (
-  <div className="w-full bg-primary-500">
-    <div className="max-w-screen-lg w-full mx-auto px-4 py-2">
-      <div>
-        
-      </div>
-      <div className="hidden md:flex flex-row justify-between max-w-lg mx-auto decoration-white">
-        <Link to={"/"} className="text-white text-lg font-bold hover:underline focus:underline">
-          Home
-        </Link>
-        <Link to={"/explore"} className="text-white text-lg font-bold hover:underline focus:underline">
-          Explore
-        </Link>
-        <span className="text-white text-lg font-bold">
-          History
+  <>
+    <div className="w-full bg-primary-700 fixed z-999">
+      <a
+        href="#main-content"
+        className="sr-only focus:not-sr-only absolute top-2 left-2 bg-blue-600 text-white px-4 py-2 rounded z-[1000]"
+      >
+        <span>
+          Skip to main content
         </span>
-        <Link to={"/login"} className="text-white text-lg font-bold hover:underline focus:underline">
-          Login
-        </Link>
+      </a>
+      <div className="max-w-screen-lg w-full mx-auto px-4 py-2 flex items-center justify-between">
+        {/* Left: Logo */}
+        <div className="flex-shrink-0">
+          <img src={logo} alt="InggrisLand" className="w-40" />
+        </div>
+    
+        {/* Center: Links */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 hidden md:flex flex-row gap-x-8">
+          <Link to="/" className="text-white text-md font-bold hover:underline focus:underline">
+            Home
+          </Link>
+          <Link to="/explore" className="text-white text-md font-bold hover:underline focus:underline">
+            Explore
+          </Link>
+          <span className="text-gray-400 text-md font-bold">History</span>
+          <Link to="/login" className="text-white text-md font-bold hover:underline focus:underline">
+            Login
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
+    <div className="h-[65px]"/>
+  </>
   )
 };
 
