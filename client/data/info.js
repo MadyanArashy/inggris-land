38d3891abd94cdf0065e65fd0f0b44b7e45ec662
MadyanArashy <madyan.arashy5@smk.belajar.id>
2025-05-03 17:13:27 +0700

    feat: add image anda data assets

---
+++ client/data/info.js
@@ -0,0 +1,15 @@
+import favicon from "../src/assets/favicon.ico"
+import logo from "../src/assets/inggrisland-logo.png"
+
+const INFO = {
+  assets: {
+    favicon: favicon,
+    logo: logo
+  },
+
+  home: {
+    
+  }
+}
+
+export default INFO;
