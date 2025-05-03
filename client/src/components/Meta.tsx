5ff7b4b42b0e2fe467d623744c743a3df20b0fd7
MadyanArashy <madyan.arashy5@smk.belajar.id>
2025-05-03 17:10:29 +0700

    feat: create dynamic title and meta tags

---
+++ client/src/components/Meta.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function Meta() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+
+    let title = "InggrisLand";
+    let favicon = "/favicon.ico";
+
+    switch (path) {
+      case "/":
+        title = "Home | InggrisLand";
+        break;
+      case "/explore":
+        title = "Explore | InggrisLand";
+        break;
+      case "/login":
+        title = "Login | InggrisLand";
+        favicon = "/favicon-login.png";
+        break;
+      default:
+        title = "InggrisLand";
+    }
+
+    document.title = title;
+
+    const link =
+      document.querySelector("link[rel*='icon']") ||
+      document.createElement("link");
+    link.setAttribute("rel", "icon");
+    link.setAttribute("href", favicon);
+    document.head.appendChild(link);
+  }, [location]);
+
+  return null;
+}
+
+export default Meta;
