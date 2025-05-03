97aa9bd33cd967347d088dda411c89d7a3bfa3cc
MadyanArashy <madyan.arashy5@smk.belajar.id>
2025-05-03 20:35:24 +0700

    fix: make home cover image responsive and put reusable section component data inside data folder

---
+++ client/data/info.ts
@@ -0,0 +1,36 @@
+const INFO = {
+  home: [
+    {
+      title: "Skills",
+      image: "/home-skills.png",
+      content: [
+        "Practise your listening, reading, writing and speaking and learn useful language to use at work or to communicate effectively with friends.",
+        "Audio and video to practise your listening and speaking skills.",
+        "Model texts for all types of writing tasks.",
+        "Work on your reading skills to read more quickly and understand more."
+      ],
+    },
+    {
+      title: "Grammar",
+      image: "/home-grammar.png",
+      content: [
+        "Revise and practise your grammar to improve your language level and increase your confidence.",
+        "Clear and simple grammar explanations to help you revise and practise different grammar points.",
+        "Online exercises to help you check your understanding of the grammar points.",
+        "Use the grammar reference for further practice and more detailed explanations."
+      ],
+    },
+    {
+      title: "Vocabulary",
+      image: "/home-vocabulary.png",
+      content: [
+        "Learn new words and improve your language level to be able to communicate in English effectively.",
+        "Online exercises to help you learn the meaning, pronunciation and spelling of new words.",
+        "Learn new words connected to a wide range of different topics.",
+        "Play our word games and have fun as you improve your vocabulary."
+      ],
+    }
+  ]
+}
+
+export default INFO;
